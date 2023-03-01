9d8651739ae026db70c07c0e3c89e35707e3919e
huy <huy>
2023-03-01 10:31:16 +0700

    Initial Commit

---
+++ hws/hwsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwsApp.swift
+//  hws
+//
+//  Created by Ly Gia Huy on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct hwsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
